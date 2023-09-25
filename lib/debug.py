5c38bcc10e64fe6693cd3e2eb1f5bb159b59080a
kevin <bettkevin965@gmail.com>
2023-09-25 20:18:44 +0300

    Update the code in my song.py and debug.py files

--- lib/debug.py
+++ lib/debug.py
@@ -6,3 +6,14 @@ from song import Song
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+    Song.create_table()
+    
+    hello = Song("Hello", "25")
+    hello.save()
+
+    despacito = Song("Despacito", "Vida")
+    despacito.save()
+
+    print("hello.id:", hello.id)
+    print("despacito.id:", despacito.id)
